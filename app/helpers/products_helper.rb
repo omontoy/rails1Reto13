113a7cda5d031d8cb550508952954268cbb871ae
Orlando Montoya <omb0320@gmail.com>
2017-01-14 18:56:48 -0500

    Crea la ruta /products con su vista

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,19 @@
 module ProductsHelper
+   def phrase(arr)
+      len = arr.length
+      str = ""
+
+      for i in 0..len-1
+         if i == len - 2
+            str = str + arr[i] + " y "
+            else
+               if i == len - 1
+                  str = str + arr[i]
+               else
+               str = str + arr[i] + ", "
+            end
+         end
+      end
+      str
+   end
 end
